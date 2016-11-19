6e4f693fcdce98ba92dbf25b453e7e7eae7322f1
oleszavadskiy <oleszavadskiy@gmail.com>
2016-11-19 11:09:20 +0200

    Module1

---
+++ Module1/FifthClass.java
@@ -0,0 +1,10 @@
+package Module1;
+
+/**
+ * Created by Олесь on 19.11.2016.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println(" Мой первый класс на джава. ");
+    }
+}
---
+++ Module1/ThirdClass.java
@@ -0,0 +1,11 @@
+package Module1;
+
+/**
+ * Created by Олесь on 19.11.2016.
+ */
+public class ThirdClass {
+    public static void main(String[] args) {
+        System.out.println("  Мой первый класс на джава. ");
+    }
+    }
+
